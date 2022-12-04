895b61343124d48a37fa3d2c4101036c5b415288
MrGab0 <gabriel.escobar.torres@gmail.com>
2022-12-03 22:09:22 -0600

    Deploy test 2

--- server/config.js
+++ server/config.js
@@ -3,5 +3,5 @@ export const PORT = 4000;
 export const DB_HOST = process.env.DB_HOST || '127.0.0.1';
 export const DB_USER = process.env.DB_USER || 'root';
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'aex1lol';
-export const DB_NAME = process.env.DB_HOST || 'escuela';
+export const DB_NAME = process.env.DB_NAME || 'escuela';
 export const DB_PORT = process.env.DB_PORT || 3306;
