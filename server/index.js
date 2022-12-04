f2c7b392a6e9a5f4a996d98000060d70db268afe
MrGab0 <gabriel.escobar.torres@gmail.com>
2022-12-03 21:50:17 -0600

    command start deploy add

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
 	cors({
-		origin: 'http://localhost:5173', // URL de donde nos conectaremos con react
+		origin: '*', // URL de donde nos conectaremos con react
 		credentials: true,
 	})
 );
